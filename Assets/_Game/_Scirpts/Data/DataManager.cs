449437d8681513681519b2e8166ffbb0ebac0707
Fjnnn2907 <phuongphipho2005@gmail.com>
2025-04-13 11:57:23 +0700

    fix bug small

---
+++ Assets/_Game/_Scirpts/Data/DataManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public static class DataManager
+{
+    public static int coin = 0;
+    public static int diamond = 0;
+
+    private static Dictionary<string, int> data = new Dictionary<string, int>()
+    {
+        { "Coin", 0 },
+        { "Diamond", 0 }
+    };
+
+    public static void SaveData()
+    {
+        PlayerPrefs.SetInt("Coin", coin);
+        PlayerPrefs.SetInt("Diamond", diamond);
+        PlayerPrefs.Save();
+        Debug.Log("Data Saved: Coin = " + coin + ", Diamond = " + diamond);
+    }
+
+    public static void LoadData()
+    {
+        coin = PlayerPrefs.GetInt("Coin", 0);
+        diamond = PlayerPrefs.GetInt("Diamond", 0);
+        Debug.Log("Data Loaded: Coin = " + coin + ", Diamond = " + diamond);
+    }
+
+    public static void ResetData()
+    {
+        PlayerPrefs.DeleteKey("Coin");
+        PlayerPrefs.DeleteKey("Diamond");
+        PlayerPrefs.Save();
+        coin = 0;
+        diamond = 0;
+        Debug.Log("Data Reset");
+    }
+}
--- Assets/_Game/_Scirpts/NickName/NickName.cs
+++ Assets/_Game/_Scirpts/NickName/NickName.cs
@@ -5,15 +5,34 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class NickName : MonoBehaviour
 {
+    public GameObject loginUI;
     public Button nickNameButton;
     public TextMeshProUGUI textName;
     public TMP_InputField playerNameInput;
     public int selectedCharacterIndex = 0;
 
+
     private void Start()
     {
         nickNameButton.onClick.AddListener(SetPlayerName);
+
+        //if (PlayerPrefs.HasKey("NickName"))
+        //{
+        //    Debug.Log("a");
+        //    string saveName = PlayerPrefs.GetString("NickName");
+        //    PhotonNetwork.NickName = saveName;
+        //    textName.text = saveName;
+        //    Debug.Log(textName.text);
+        //    loginUI.SetActive(false);
+        //}
+        //else
+        //{
+        //    PopUpLogin(); 
+        //}
+
+        PopUpLogin();
     }
+
 
     public void SetPlayerName()
     {
@@ -24,6 +43,13 @@ public class NickName : MonoBehaviour
             PhotonNetwork.NickName = playerNameInput.text;
             textName.text = playerNameInput.text;
         }
+    }
+
+    public void PopUpLogin()
+    {
+        bool hasNickname = PhotonNetwork.NickName == "";
+        textName.text = PhotonNetwork.NickName;
+        loginUI.SetActive(hasNickname);
     }
 
     public void SelectCharacter(int index)
